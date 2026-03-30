071a86e9deb466e1d7749cc3e51473729bdc4df5
Abdullah Ali <abdullahabdulnaserali@gmail.com>
2026-03-30 13:11:03 +0300

    create a dictionary with the name expense to include the description,amount and data

---
+++ main.py
@@ -0,0 +1,16 @@
+expenses =[]
+
+def add_expenses():
+    description = input("Enter expenses description: ")
+    date = input("Enter expenses date (YYYY-MM-DD): ")
+    amount = float(input("Enter expenses amount: "))
+
+
+    expense={
+        "description": despcription,
+        "date": date,
+        "amount": amount
+    }
+
+    
+
